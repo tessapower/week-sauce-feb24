4c573a407b98c66204ce023dfae0c6cbbcdca333
Tessa Power <hello@tessapower.xyz>
2024-02-05 19:28:47 -0600

    Replace Area2D spawn area with ReferenceRect

--- scripts/managers/mole_spawner.gd
+++ scripts/managers/mole_spawner.gd
@@ -2,7 +2,10 @@ extends Node
 
 # ====================Public Interface====================
 
-@export var spawn_area: Area2D
+## A [code]ReferenceRect[/code] which outlines where moles can be spawned.
+@export var spawn_area: ReferenceRect
+
+## The mole scene to be spawned.
 @export var mole_scene: PackedScene
 
 signal mole_spawned(mole: Mole, position: Vector2)
@@ -16,9 +19,17 @@ func spawn_mole():
 
 # ----------Inherited from parent----------
 
+var spawn_area_start: Vector2
+var spawn_area_end: Vector2
+
 func _ready():
 	# Assertions
 	assert(mole_scene != null)
+	assert(spawn_area != null)
+	spawn_area_start = spawn_area.get_global_position()
+	spawn_area_end = spawn_area_start + spawn_area.size
+
+
 
 # ----------Callbacks----------
 
@@ -29,5 +40,5 @@ func _on_timer_timeout():
 
 func _get_random_spawn_point() -> Vector2:
 	return Vector2(
-		randf_range(spawn_area.position.x, spawn_area.end.x),
-		randf_range(spawn_area.position.y, spawn_area.end.y))
+		randf_range(spawn_area_start.x, spawn_area_end.x),
+		randf_range(spawn_area_start.y, spawn_area_end.y))
