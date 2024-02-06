bc3bbb6f2a46eb3e25d4a1af852c50ae6f2c7643
Phuwasate Lutchanont <115549144+LongcatOwO@users.noreply.github.com>
2024-02-07 07:57:30 +1300

    Implemented basic mole stats and resource (#52)

--- scripts/mole/mole.gd
+++ scripts/mole/mole.gd
@@ -1,12 +1,34 @@
 class_name Mole extends CharacterBody2D
-## mole.gd: Controls the mole enemy character.
+## mole.gd: Represents the mole enemy character
+##			Interface includes: controlling the mole, interaction with its stats.
 ##
-## Author(s): Tessa Power
+## Author(s): Tessa Power, Phuwasate Lutchanont
 
 ## Animations
 @onready var animated_sprite = $AnimatedSprite2D
 
+@export var data: MoleData
+
+
+signal mole_died(data: MoleData)
+
+
+var max_health: int:
+	get: return _max_health
+
+var current_health: int:
+	get: return _current_health
+
+
+func apply_damage(value: int) -> void:
+	_current_health = max(0, _current_health - value)
+	if _current_health == 0: mole_died.emit(data)
+
+
 func _ready() -> void:
+	assert(data != null)
+	_load_data()
+
 	animated_sprite.play("emerge")
 
 
@@ -16,3 +38,13 @@ func _on_animation_finished() -> void:
 	match animated_sprite.animation:
 		"emerge":
 			animated_sprite.play("idle")
+
+
+# Initialzes mole stats from the data
+func _load_data() -> void:
+	_max_health = data.max_health
+	_current_health = _max_health
+
+
+var _current_health: int
+var _max_health: int
