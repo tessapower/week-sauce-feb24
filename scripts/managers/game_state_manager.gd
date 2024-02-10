b0a53fc171259a1ac25b5f12479b5b04412f7fa4
Tessa Power <hello@tessapower.xyz>
2024-02-09 21:51:10 -0600

    Add level picker to start menu and support loading level scene through game state manager

--- scripts/managers/game_state_manager.gd
+++ scripts/managers/game_state_manager.gd
@@ -19,6 +19,24 @@ func initialize_for_scene() -> void:
 	_reset_time()
 	is_paused = false
 
+# ----------Level Scene----------
+
+enum LEVEL {grassy_field = 0, desert, underwater}
+var level_scenes: Dictionary = {
+	LEVEL.grassy_field: "res://scenes/levels/grassy_field.tscn",
+	LEVEL.desert: "res://scenes/levels/desert.tscn",
+	LEVEL.underwater: "res://scenes/levels/underwater.tscn"
+}
+# Set default level to grassy field
+var current_level: String = level_scenes.get(LEVEL.grassy_field)
+
+func set_level(level: LEVEL) -> void:
+	current_level = level_scenes.get(level)
+
+
+func load_current_level() -> void:
+	get_tree().change_scene_to_file(current_level)
+
 # ----------Player Health----------
 
 signal player_health_changed(new_value: int)
