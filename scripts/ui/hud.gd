92f2051b44d1ff615fcffc2f06f48064623ff8c4
Tessa Power <hello@tessapower.xyz>
2024-02-11 15:44:06 -0600

    Refactor HUD to use flashing label

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -1,20 +1,16 @@
 class_name Hud extends Control
-# hud.gd: Controls the display of the game's HUD
 
-# Author(s): Phuwasate Lutchanont
+## hud.gd: Controls the display of the game's HUD
+##
+## Author(s): Phuwasate Lutchanont, Tessa Power
 
-@export var level_label: Label
-@export var health_bar: Range
-@export var exp_bar: Range
-@export var score_label: Label
+@onready var level_label = get_node("Content/BottomUI/FlashingLabel")
+@onready var health_bar: Range = get_node("Content/BottomUI/HealthAndXP/HealthBar")
+@onready var exp_bar: Range = get_node("Content/BottomUI/HealthAndXP/ExperienceBar")
+@onready var score_label: Label = get_node("Content/ScoreLabel")
 
 
 func _ready() -> void:
-	assert(level_label != null)
-	assert(health_bar != null)
-	assert(exp_bar != null)
-	assert(score_label != null)
-
 	game_state_manager.player_level_changed.connect(_on_player_level_changed)
 	game_state_manager.player_health_changed.connect(_on_player_health_changed)
 	game_state_manager.player_max_health_changed.connect(_on_player_max_health_changed)
@@ -22,20 +18,28 @@ func _ready() -> void:
 	game_state_manager.player_max_exp_changed.connect(_on_player_max_exp_changed)
 	game_state_manager.score_changed.connect(_on_score_changed)
 
+
 func _on_player_level_changed(new_value: int) -> void:
-	level_label.text = "LV %02d" % new_value
+	level_label.text = "LVL %03d" % new_value
+	if new_value > 1:
+		level_label.flash()
+
 
 func _on_player_health_changed(new_value: int) -> void:
 	health_bar.value = new_value
 
+
 func _on_player_max_health_changed(new_value: int) -> void:
 	health_bar.max_value = new_value
+
 
 func _on_player_exp_changed(new_value: int) -> void:
 	exp_bar.value = new_value
 
+
 func _on_player_max_exp_changed(new_value: int) -> void:
 	exp_bar.max_value = new_value
+
 
 func _on_score_changed(new_value: int) -> void:
 	score_label.text = str(new_value)
