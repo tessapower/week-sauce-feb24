eb48ca79730f22da8c32301a62da53a36d735ee8
Tessa Power <hello@tessapower.xyz>
2024-02-12 12:58:17 -0600

    Support spawning golden moles

--- scripts/managers/mole_spawner.gd
+++ scripts/managers/mole_spawner.gd
@@ -15,6 +15,8 @@ extends Node
 
 ## The mole scene to be spawned.
 @export var mole_scene: PackedScene
+const GOLDEN_MOLE = preload("res://scenes/mole/golden_mole.tscn")
+@export_range(0.0, 1.0, 0.05) var chance_of_golden_mole = 0.1
 
 const BUFFER_AREA = preload("res://scenes/mole/buffer_area.tscn")
 @onready var buffer = BUFFER_AREA.instantiate()
@@ -22,8 +24,10 @@ const BUFFER_AREA = preload("res://scenes/mole/buffer_area.tscn")
 signal mole_spawned(mole: Mole, position: Vector2)
 
 func spawn_mole():
-	mole_spawned.emit(
-		mole_scene.instantiate() as Mole, buffer.global_position)
+	if randf() < chance_of_golden_mole:
+		mole_spawned.emit(GOLDEN_MOLE.instantiate() as Mole, buffer.global_position)
+	else:
+		mole_spawned.emit(mole_scene.instantiate() as Mole, buffer.global_position)
 
 # ====================Internal details====================
 
