3875a8f8bdadb786fc1213fc902179e0b1af6604
Tessa Power <hello@tessapower.xyz>
2024-02-13 00:07:32 -0600

    Add comments to power up popup script

--- scripts/powerups/power_up_selector.gd
+++ scripts/powerups/power_up_selector.gd
@@ -1,19 +1,18 @@
 extends PopupPanel
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Get 2 random powerups from power up system
+	# Set the power up icon as the skill texture button textures
+	# Keep track of which skill is associated with which texture button
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_random_skill_1_pressed():
+	# apply this skill
 	pass # Replace with function body.
 
 
 func _on_random_skill_2_pressed():
+	# apply this skill
 	pass # Replace with function body.
